fd659aa9018b3493aac13e4322cd7b3a36894e8f
CipiVlad <supercoder.cipi@gmail.com>
2022-05-10 14:11:40 +0200

    Übungen

---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+
+// BOOLEAN COMPARISONS
+
+// ----lev-1_14
+
+// console.log(Boolean(100))
+
+// console.log(Boolean(3.14))
+
+// console.log(Boolean(-15))
+
+// console.log(Boolean("hello"))
+// console.log(Boolean("false"))
+// console.log(Boolean(1 + 7 + 3.14))
+// console.log(Boolean(0))
+// console.log(Boolean(-0))
+// console.log(Boolean(NaN))
+// console.log(Boolean(null))
+// console.log(Boolean(false))
+// console.log(Boolean(true))
+
+// ----lev-1_15
+
+// let num = 5;
+// let anotherNum = 8;
+// console.log(num == anotherNum);
+// console.log(num > anotherNum);
+// console.log(num < anotherNum);
+// console.log(num >= anotherNum);
+// console.log(num <= anotherNum);
+
+// console.log(num == "5");
+// console.log(num != "8");
+// console.log(num != "5");
+
+
+
+// DATA TYPES
+// --------lev 1_3-----Einführung data types------------------
+// let i = 1;
+// console.log(typeof (1));/* number*/
+// let lastName = "Johnson";
+// console.log(typeof ("Johnson"));/* string*/
+// let j = "2";
+// console.log(typeof ("2"));/* string*/
+// let status = true;
+// console.log(typeof (status));/* boolean*/
+// let hello = "hello";
+// console.log(typeof (hello));/* string*/
+// let helloWorld = "hello";
+// console.log(typeof (helloWorld));/* string*/
+
+
+
+
+// --------lev 1_4------Einführung data types------------------
+
+// console.log(typeof ("John"));/* string*/
+// console.log(typeof (3.14));/* number*/
+// console.log(typeof (NaN)); /* number*/
+// console.log(typeof (false)); /* boolean*/
+// console.log(typeof ([1, 2, 3, 4])); /* object*/
+// console.log({ name: 'john', age: 34 });/* object*/
+// console.log(typeof (function myFunction() { }));/* object*/
+// console.log(typeof (null));/* object*/
+// console.log(typeof (""));/* string*/
+// console.log(typeof (2 + 3 == 5));/* boolean*/
+// console.log(typeof (3 + "3"));/* string*/
+
+
+// --------lev 1_8------Einführung data types------------------
+
+// // Aufgabenstellung 1
+// let x = 20;
+// let y = 30;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+
+// let z = 10;
+// let resultOne = (x * y) / z;
+// console.log(resultOne);
+
+// // Aufgabenstellung 2
+
+// let a = 15;
+// let b = 9;
+// console.log(a % b); /*Restwert 6*/
+
+// let c = 20;
+// let resultTwo = ((a + b) * c)
+// console.log(resultTwo);
+
+// let resultThree = b - a;
+// console.log(resultThree);
+
+// console.log(resultOne % resultTwo);
